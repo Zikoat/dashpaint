af5adc16f63bc5f48a82540fed9740638f392424
Sigurd Schoeler <sigurd@finexa.no>
2022-09-12 18:36:24 +0200

    Refresh game when changes are made to the code

--- src/ReactGame.tsx
+++ src/ReactGame.tsx
@@ -1,4 +1,13 @@
 import "../src/PhaserGame";
+
+import { DashPaintGame } from "../src/PhaserGame";
+
+// https://nextjs.org/docs/basic-features/fast-refresh
+// > If you edit a file with exports that aren't React components, Fast Refresh
+// > will re-run both that file, and the other files importing it.
+
+export const forceRefresh = DashPaintGame;
+
 export default function ReactGame(): JSX.Element {
   return <></>;
 }
