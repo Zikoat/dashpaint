2dcfa95ea22480245ae163f74281453f7ec53ea0
Sigurd Schoeler <sschoeler99@gmail.com>
2023-03-13 20:58:51 +0100

    Fix config

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import "phaser";
 import DashPaintPage from "./components/DashPaintPage";
-import { DashPaintConfig } from "./PhaserGame";
+import { DashPaintConfig } from "./DashPaintConfig";
 import { Game } from "phaser";
-
-window.addEventListener("load", () => {
-  window.game = new Game(DashPaintConfig);
-});
 
 declare global {
   interface Window {
     game: Phaser.Game;
   }
 }
+
+window.addEventListener("load", () => {
+  window.game = new Game(DashPaintConfig);
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
